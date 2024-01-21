26c83bb15aeae9f263c616b169afcfe5beeba65b
Elliot Kang <kkang2097@gmail.com>
2024-01-20 19:25:03 -0800

    Update lib_example.py

--- agent_lite/lib_example.py
+++ agent_lite/lib_example.py
@@ -1,12 +1,40 @@
-
+from functools import reduce, partial
 from agent_lite import Resource
+from pydantic import BaseModel
 
-def basic_node(context):
+#TODO: Turn context's type into agent_lite.Context type later
+def basic_node(context: BaseModel):
     def chain_action(statement):
-        print(context)
+        print(1)
+        return context
     return chain_action
+
+def basic_node2(context: BaseModel):
+    def chain_action(statement, arg2):
+        print(2)
+    return chain_action
+
+def pipe(*functions):
+    return reduce(lambda f, g: lambda x: g(f(x)), functions, lambda x: x)
 
 if __name__ == "__main__":
     resource_obj = Resource(num_threads = 4)
-    basic_node(resource_obj)("Something") 
+    #basic_node(resource_obj)("Something")
+
+    #Then we can try chaining these two
+    #1. Define the chain 
+    chain_dict = {
+        1: basic_node,
+        2: basic_node2
+    }
+    #2. Insert Context 
+    #Alternatively, we can do this with a list as well
+    chain_dict = {k: v(resource_obj) for k, v in chain_dict.items()}
+    chain_dict[2] = partial(chain_dict[2], arg2 = 1)
+    #3. Pipe functions
+    #chain_dict[2](chain_dict[1]("HEllo"))
+    composed_chain = pipe(*chain_dict.values())
+    
+    #This should print "context" argument twice
+    composed_chain("Hello?")
     print("hello world!") 
