f40cc0d72b9442f1dac70a357b8efbc004e37629
Elliot Kang <kkang2097@gmail.com>
2024-01-25 16:31:35 -0800

    Update basic.py

--- agent_lite/agent_lite/chains/basic.py
+++ agent_lite/agent_lite/chains/basic.py
@@ -3,3 +3,17 @@
 def basic_chain(context):
     def chain_func(arguments):
         print("hello")
+
+
+#TODO: Chain regular functions
+
+
+
+#TODO: Chain async functions
+
+
+
+#TODO: Write 1 function that will do both async and regular functions
+
+if __name__ == "__main__":
+    print("hello world")
