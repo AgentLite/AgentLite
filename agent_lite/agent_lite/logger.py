7bfeeeff62193c701b51681f00121254250435cc
Elliot Kang <kkang2097@gmail.com>
2024-01-25 16:42:05 -0800

    Update logger.py

--- agent_lite/agent_lite/logger.py
+++ agent_lite/agent_lite/logger.py
@@ -4,8 +4,21 @@ from abc import ABC, abstractmethod
 '''
     Logger will send an event object to some data store (Kafka, RDB, etc)
 '''
-def Logger(ABC):
+class Logger(ABC):
 
     @abstractmethod  
-    def log(event: Object):
+    def log(event: Object, sources: Object):
+        '''
+        event: Event Object
+        sources: Dict of sources to log to (Kafka, RDB, etc)
+        '''
         pass
+
+class PrintLogger(Logger):
+    def log({'message': "something", 'time': "now"}, sources: {'kafka': None}):
+        #Send message to kafka 
+        pass
+
+
+if __name__ == "__main__":
+    #TODO: Try doing this with print statements first
