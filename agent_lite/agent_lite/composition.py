161fa208dee48efcb75f519f23223ef1a487c7a0
Elliot Kang <kkang2097@gmail.com>
2024-01-25 23:33:42 -0800

    Create composition.py

---
+++ agent_lite/agent_lite/composition.py
@@ -0,0 +1,26 @@
+import functools
+
+#TODO: Add functional parts for higher order function serialization, etc.
+
+
+#TODO: Compose synchronous functions
+
+
+
+
+#TODO: Compose async functions
+
+
+#TODO: Unroll higher-order recursive functions to make them serializable
+
+
+#TODO: Serialize any function
+
+
+
+
+if __name__ == "__main__":
+    #TODO: Run mini-experiments on testing these
+
+
+    #TODO: Export tests to /tests folder
