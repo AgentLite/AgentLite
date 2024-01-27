2fc88540cce3b2c23028e7c5bb834cb680d03174
Elliot Kang <kkang2097@gmail.com>
2024-01-26 23:03:19 -0800

    async function chaining

--- agent_lite/agent_lite/composition.py
+++ agent_lite/agent_lite/composition.py
@@ -1,14 +1,27 @@
-import functools
+from functools import reduce
+import asyncio
+import inspect
+import dill
 
 #TODO: Add functional parts for higher order function serialization, etc.
 
 
 #TODO: Compose synchronous functions
-
-
-
+def pipe(*functions):
+    return(reduce(lambda f, g: lambda x: g(f(x)), functions))
 
 #TODO: Compose async functions
+
+async def async_pipe(*functions):
+    return
+
+def async_lambda(f, g):
+    async def child_f(x):
+        return await g(await f(x))
+    return(lambda x: child_f(x))
+
+def apipe(*functions):
+    return(reduce(async_lambda, functions)) 
 
 
 #TODO: Unroll higher-order recursive functions to make them serializable
@@ -19,8 +32,31 @@ import functools
 
 
 
+
+#TODO: Testing Grounds
+async def plus1(x):
+    print("PLEASE")
+    print(x)
+    return x + 1
+async def print1(x):
+    print(x)
+    print("CHECK CONCURRENCY")
+    return x + 1
+async def print2(x):
+    print("Hello")
+    return
+
+
+
 if __name__ == "__main__":
     #TODO: Run mini-experiments on testing these
+    async def trythis():
+        exp = apipe(plus1, print1)
+        print(inspect.iscoroutinefunction(exp))
+        print(await exp(2))
+        return
+    #print(apipe(plus1, print1))
 
-
+    #See, it still runs with asyncio.run :)
+    asyncio.run(apipe(plus1, print1, print2)(1))
     #TODO: Export tests to /tests folder
