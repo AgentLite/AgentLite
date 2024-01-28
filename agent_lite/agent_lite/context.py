aefb745106ed11a5dc7f64185e7959582319724f
Elliot Kang <kkang2097@gmail.com>
2024-01-27 22:07:06 -0800

    Update context.py

--- agent_lite/agent_lite/context.py
+++ agent_lite/agent_lite/context.py
@@ -1,7 +1,7 @@
 from pydantic import BaseModel
+from typing import Any
 
 class Context(BaseModel):
     model: Any #This should be a Model object
     logger: Any #This should be a Logger object 
-    resources: Any #This should be a Resource object 
     stores: Any #This should be a Stores object
