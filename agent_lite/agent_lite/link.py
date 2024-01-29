797be8317e71326f01764af2295de8de26e89602
Elliot Kang <kkang2097@gmail.com>
2024-01-29 01:38:12 -0800

    Update link.py

--- agent_lite/agent_lite/link.py
+++ agent_lite/agent_lite/link.py
@@ -7,10 +7,17 @@ T = TypeVar("T")
     A chain is made up of Links.
 '''
 class Link(BaseModel):
+    #TODO: Add union type for Union(Context | dict[str, Any])
     context: T 
 
     @abstractmethod
     def forward(self):
+        '''
+            Main method for executing the Link in the LLM chain
+        '''
+        pass
+
+    async def a_forward(self):
         '''
             Main method for executing the Link in the LLM chain
         '''
