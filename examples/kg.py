50a0bb88d3aa7810e05f6ccaf1d84dd025e20153
Elliot Kang <kkang2097@gmail.com>
2024-01-29 01:38:22 -0800

    Update kg.py

--- examples/kg.py
+++ examples/kg.py
@@ -1,24 +1,25 @@
-from agent_lite import Tool, Context, Link, Prompt, apipe
+from agent_lite import OpenAIModel, Tool, Context, Link, Prompt, apipe
 from pydantic import BaseModel
 
 #TODO: Neo4j KG store
 class KGStore(BaseModel):
-
+    #TODO: Think about whether we use the API key or the client
+    api_key: str 
     def insert(entity: str, relationship: str):
         #TODO: Do this
         pass
 
 #TODO: Context
+stores = {'kg': None}
+context = Context()
+
+
+#TODO: Link 1: Named Entity Recognition with LLM 
 
 
 
-#TODO: NER tool 
 
-
-
-
-
-#TODO: Link
+#TODO: Link 2: Use prompt template to generate triplets + place into Neo4j DB
 
 
 
