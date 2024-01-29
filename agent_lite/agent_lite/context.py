c2bb7754999a146487015b2318f320403ba05abb
Elliot Kang <kkang2097@gmail.com>
2024-01-29 01:07:58 -0800

    add tools to context

--- agent_lite/agent_lite/context.py
+++ agent_lite/agent_lite/context.py
@@ -1,7 +1,12 @@
 from pydantic import BaseModel
 from typing import Any
+from .tool import Tool
+
 
 class Context(BaseModel):
     model: Any #This should be a Model object
     logger: Any #This should be a Logger object 
     stores: Any #This should be a Stores object
+    tools: dict[str, Tool]
+
+
