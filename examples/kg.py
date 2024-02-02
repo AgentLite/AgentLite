b44db85e43273044ccc48d41199b38bae0654b70
Elliot Kang <kkang2097@gmail.com>
2024-02-01 20:39:08 -0800

    Update kg.py

--- examples/kg.py
+++ examples/kg.py
@@ -24,6 +24,7 @@ products = [
         "name": "Babolat Pure Aero 98",
         "MSRP": "$279.99",
         "Dealer Price": "$189.99",
+        "url": "https://www.tennis-warehouse.com/Babolat_Pure_Aero_98_2023/descpageRCBAB-BARO98.html?from=gpmax&gad_source=1&gclid=Cj0KCQiAn-2tBhDVARIsAGmStVnoV-zo5Ma8xvFJ4VnRWJuP4tkZny9h_Dl5-mcDg9akT3OPa4p9dV0aAqKcEALw_wcB",
         #The description is long, but maybe we can use MapReduce or some filtering algorithm to truncate the description 
         "Description": "Babolat adds another chapter to the most controlled and surgical member of the Pure Aero family. Formerly called the Pure Aero VS, the Pure Aero 98 gives big hitters a powerfully precise weapon. Compared to the standard Pure Aero, this racquet features a more compact head along with a denser string pattern and a thinner, more flexible beam. The payoff is extra control, which comes in handy on bigger cuts or when targeting the lines. That said, this stick still packs enough pop to put your opponent in check. For 2023 Babolat adds NF² Technology which uses flax inserts at 3/9 in the head to soften ball impact. Babolat also updates its FSI Spin technology with slightly tighter string spacing. The result is greater control (and confidence) when maximizing stroke speed in the service of power and spin. With some nice updates to the response, the Pure Aero 98 delivers the spin and power of a modern player's racquet but with the control and feel needed to fully engage with your targets."
     },
@@ -31,6 +32,7 @@ products = [
         "name": "Babolat Pure Drive",
         "MSRP": "$259.99",
         "Dealer Price": "$189.99",
+        "url": "https://www.tennis-warehouse.com/Babolat_Pure_Drive/descpageRCBAB-BRPDR.html?from=gshop&gad_source=1&gclid=Cj0KCQiAn-2tBhDVARIsAGmStVkWu38KImV2XwMsZ4S9KQUKSYN7dZy-K7DXgvIpkuRQ6l1L5KZ0XaEaAqOBEALw_wcB",
         "Description": "Babolat adds another chapter to the game's most iconic modern player's racquet. As with previous versions of the Pure Drive, this update keeps the same easy learning curve and explosive spec profile that has attracted generations of players. Updated with Babolat's new HTR System, the Pure Drive's graphite layup has been reengineered for higher torsional rigidity, resulting in more efficient energy transfer to the ball. To help with vibration dampening, Babolat has devoted more space in the shaft to SWX Pure Feel, a thin and extremely flexible viscoelastic rubber that sits between the racquet's carbon layers. Additional features include FSI Power Technology, which optimizes the grommet system and string spacing for higher levels of power and spin. On court, this racquet's explosive acceleration and spin-friendly targeting make it a dangerous weapon from the baseline. It also packs impressive all-court mobility, making it great for cranking winners on the run or charging into the front court and finishing points at net. With incremental tweaks to the feel, this update won't disappoint especially if you place a premium on quick handling. The Pure Drive continues to be one of the best options for those who want a light and explosive player's racquet."
     }
 ]
@@ -89,14 +91,12 @@ Sometimes the LLM will output <"Babolat Pure Aero 98", "SELLS", "TENNIS WAREHOUS
 if __name__ == "__main__":
     #TODO: Execution code for simple LLM agent 
     print(kg_prompt(products[0], nodes, relations)) 
-    
-    async_session = aiohttp.ClientSession()
     async def try_this():
-        async_session = aiohttp.ClientSession()
         llm_args = {
             'model': 'gpt-3.5-turbo',
             'temperature': 0.1
         }
+        async_session = aiohttp.ClientSession()
         #Hard-coded Triplet generation
         llm = OpenAIModel(async_session = async_session, llm_args = llm_args, api_key = os.environ.get('OPENAI_API_TOKEN', ""))
         output = await llm.async_predict(kg_prompt(products[0], nodes, relations))
